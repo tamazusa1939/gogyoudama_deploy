2f7aedd090a49d65957ffa38e184931e92366ddd
tamazusa1939 <157197925+tamazusa1939@users.noreply.github.com>
2026-09-12 13:53:32 +0900

    Deploy 2026/09/12 13:53:32.58

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "tzmazusa-GoggyousatsuOnline-1.1.5";
+const cacheName = "tzmazusa-GoggyousatsuOnline-1.1.6";
 const contentToCache = [
-    "https://storage.googleapis.com/gogyoudama/Build/839cf1333a4343da7d61b0b028bb615b.loader.js",
+    "https://storage.googleapis.com/gogyoudama/Build/d52216914efdec5a4ab01a462f9bc514.loader.js",
     "https://storage.googleapis.com/gogyoudama/Build/eed033890c2bdc9bfc5919cf006d15dc.framework.js.unityweb",
-    "https://storage.googleapis.com/gogyoudama/Build/21adae5ff2e7842e59f63700a68c5cdb.data.unityweb",
-    "https://storage.googleapis.com/gogyoudama/Build/2855c38c8f1b38f3f7bc5ed6d1e29ba4.wasm.unityweb",
+    "https://storage.googleapis.com/gogyoudama/Build/9f15b3b1829be8f74ec365808d5b2276.data.unityweb",
+    "https://storage.googleapis.com/gogyoudama/Build/324f84cddf9307b6b90b7b28f4aee904.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
